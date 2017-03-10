ebdc4088974ba1236da156a122ccc44002f3df3c
Siddhartha Nutulapati <sidgonuts@gmail.com>
2017-03-10 07:58:53 -0500

    further scaffolding

--- sim_tournament.py
+++ sim_tournament.py
@@ -1,6 +1,6 @@
 # File: sim_tournament.py
 # Author: Siddhartha Nutulapati
-# 
+#
 # Goal: simulate a march madness tournament based solely on the seeding
 # of the inital teams, assuming that each team wins with a probability
 # proportional to the difference in seeding
@@ -9,7 +9,26 @@ import csv
 import math
 
 def main():
+    # read in file
+    # init tournament
+    # sim tournament
     return 0
 
+
+def read_file():
+    return 0
+
+
+def generate_bracket():
+    return 0
+
+
+def sim_tournament():
+    # until bracket length == 4, sim games
+    return 0
+
+
+def sim_games():
+    return 0
 
 
