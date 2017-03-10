442566baf93315073616ac740c3516af156937b3
Siddhartha Nutulapati <sidgonuts@gmail.com>
2017-03-10 18:30:35 -0500

    plot irl final 4 seeds

--- sim_tournament.py
+++ sim_tournament.py
@@ -6,17 +6,32 @@
 # proportional to the difference in seeding
 
 import csv
-import math
+import matplotlib.pyplot as plt
 
 def main():
     # read in file
+    f4irl = read_file()
     # init tournament
     # sim tournament
+    # plot results
+    plot(f4irl, [])
     return 0
 
 
 def read_file():
-    return 0
+    # parse through, find seeds of final four for evey year
+    file = open('NCAA Mens March Madness Historical Results.csv')
+    array = []
+    csv_reader = csv.reader(file)
+    line = 0
+    for row in csv_reader:  # read in all rows from csv file
+        line += 1
+        if line == 1:  # ignore first row, that's the column names
+            continue  # skip rest of the code for the first row
+        if row[1] == 'Elite Eight':
+            array.append(int(row[3]))
+
+    return array
 
 
 def generate_bracket():
@@ -28,7 +43,19 @@ def sim_tournament():
     return 0
 
 
-def sim_games():
+def sim_round():
     return 0
 
 
+def sim_game(high, low):
+    return 0
+
+
+def plot(irl, sim):
+    plt.hist(irl, bins='auto')
+    plt.show()
+    return 0
+
+
+
+main()
