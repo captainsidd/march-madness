8f7f7c5766071b29c1ff5322f50af7ba8b7a9399
Siddhartha Nutulapati <sidgonuts@gmail.com>
2017-03-10 07:48:49 -0500

    init sim_tournament program

---
+++ sim_tournament.py
@@ -0,0 +1,15 @@
+# File: sim_tournament.py
+# Author: Siddhartha Nutulapati
+# 
+# Goal: simulate a march madness tournament based solely on the seeding
+# of the inital teams, assuming that each team wins with a probability
+# proportional to the difference in seeding
+
+import csv
+import math
+
+def main():
+    return 0
+
+
+
